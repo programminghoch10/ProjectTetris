f74fbe1e9681f2e4e9bf742185a6e651e8a90083
programminghoch10 <hoch10@kabelbw.de>
2019-06-29 01:35:13 +0200

    added temporary matrix

--- src/CT12/backend.java
+++ src/CT12/backend.java
@@ -1,10 +1,31 @@
 package CT12;
 
 public class backend {
-
+	
+	private static String gmi = "bl-n-n"; //gamematrix initial value for every cell for testing
+	public static String[][] gamematrix = {
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+			{gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi,gmi},
+	};
+	// only predefined like this until there is a proper initialization within the backend main function
+	// also comes in handy for testing connection to ui class
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- src/CT12/ui.java
+++ src/CT12/ui.java
@@ -1,10 +1,10 @@
 package CT12;
 
 public class ui {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.print(backend.gamematrix[1][1].split("-")[0]);
 	}
 
 }
