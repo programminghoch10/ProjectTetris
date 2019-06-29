69011a877296a5491326c1f3aa1f512237e3b0df
jjw2202 <0.spam.adresse.0@gmail.com>
2019-06-29 16:02:57 +0200

    add a window

--- src/CT12/ui.java
+++ src/CT12/ui.java
@@ -1,16 +1,32 @@
 package CT12;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class ui {
+	
+	//=========================================================
+	
+	
+	
+	//=========================================================
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print(backend.gamematrix[1][1].split("-")[0]);
 		
-		JFrame fenster = new JFrame("Tetris Game CT12"); 	//Neues Frame (Fenster)
-		fenster.setSize(600, 400);		//Startgroesse festlegen
-		fenster.setVisible(true);		//Fenster wird sichtbar gemacht
-	}
+		JFrame fenster = new JFrame("Tetris Game CT12"); 				//create new Frame
+		fenster.setSize(600, 1000);										//set start size 		
+		fenster.setVisible(true);										//Window becomes visible
+		
+		
+		//==========================================================
+		
+		
+		
+		//==========================================================
 
+	}
+	
+	
 }
