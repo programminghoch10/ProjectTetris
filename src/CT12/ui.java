fc73ed7c80b838da20693d5fc419c952154ecd5b
jjw2202 <0.spam.adresse.0@gmail.com>
2019-06-29 13:19:57 +0200

    Es wird ein leeres Fenster erstellt

--- src/CT12/ui.java
+++ src/CT12/ui.java
@@ -1,10 +1,16 @@
 package CT12;
 
+import javax.swing.JFrame;
+
 public class ui {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print(backend.gamematrix[1][1].split("-")[0]);
+		
+		JFrame fenster = new JFrame("Tetris Game CT12"); 	//Neues Frame (Fenster)
+		fenster.setSize(600, 400);		//Startgroesse festlegen
+		fenster.setVisible(true);		//Fenster wird sichtbar gemacht
 	}
 
 }
