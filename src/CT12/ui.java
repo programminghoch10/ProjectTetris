681c43ba2593f3e6655f434587c2075bfd239a0e
programminghoch10 <hoch10@kabelbw.de>
2019-06-29 17:31:13 +0200

    added gridlayout and random boxes

--- src/CT12/ui.java
+++ src/CT12/ui.java
@@ -15,10 +15,36 @@ public class ui {
 		// TODO Auto-generated method stub
 		//System.out.print(backend.gamematrix[1][1].split("-")[0]);
 		
-		JFrame fenster = new JFrame("Tetris Game CT12"); 				//create new Frame
-		fenster.setSize(600, 1000);										//set start size 		
-		fenster.setVisible(true);										//Window becomes visible
-		
+		JFrame frame = new JFrame("Tetris Game CT12"); 				//create new Frame
+		frame.setSize(600, 1000);										//set start size 		
+		frame.setVisible(true);										//Window becomes visible
+		frame.setLayout(new GridLayout(15, 10));
+	    for (int i=0; i<15*10; i++) {
+	    	JPanel J = new JPanel();
+	    	switch ((int)(Math.random()*6)) {
+	    	case 0:
+	    		J.setBackground(Color.BLACK);
+	    		break;
+	    	case 1:
+	    		J.setBackground(Color.GREEN);
+	    		break;
+	    	case 2:
+	    		J.setBackground(Color.BLUE);
+	    		break;
+	    	case 3:
+	    		J.setBackground(Color.MAGENTA);
+	    		break;
+	    	case 4:
+	    		J.setBackground(Color.RED);
+	    		break;
+	    	case 5:
+	    		J.setBackground(Color.ORANGE);
+	    		break;
+	    	}
+	    	frame.add(J);
+	    }
+	    frame.pack();
+	    
 		
 		//==========================================================
 		
